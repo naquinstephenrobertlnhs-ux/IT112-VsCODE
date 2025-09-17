b0674546f898da552cec1abe528a05bc8ee95cab
naquinstephenrobertlnhs-ux <naquin.stephenrobert.lnhs@gmail.com>
2025-09-17 22:01:26 +0800

    First Commit

---
+++ lab_exercise1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+    //printing "C PROGRAMMING" in asterisk pattern
+printf(" ****    ****    ****    ***   ***   ****    ***   *     *  *     *  ***  *   *   *** \n");
+printf("*        *    *  *   *  *   * *      *   *  *   *  **   **  **   **   *   **  *  *    \n");
+printf("*        *    *  *   *  *   * *  **  *   *  *   *  * * * *  * * * *   *   * * *  *  **\n");
+printf("*        ****    ****   *   * *   *  ****   *****  *  *  *  *  *  *   *   *  **  *   *\n");   
+printf(" ****    *       *   *   ***   ***   *   *  *   *  *     *  *     *  ***  *   *   *** \n");
+
+    printf("\n");
+
+    //printing last name
+printf("*   *   ***    ****   *   *  ***  *   *\n");
+printf("**  *  *   *  *    *  *   *   *   **  *\n");
+printf("* * *  *   *  *    *  *   *   *   * * *\n");
+printf("*  **  *****  *   **  *   *   *   *  **\n");
+printf("*   *  *   *   **** *  ***   ***  *   *\n");
+return 0;
+}
